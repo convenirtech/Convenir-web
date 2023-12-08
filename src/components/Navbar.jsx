afcb3cf42e20d8b052851bf3cb7acad6c7ddb17b
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-12-09 01:20:49 +0200

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -32,25 +33,27 @@ const Navbar = () => {
             </button>
             <button className=" hover:text-[#ca2124]">Catalog</button>
             <button className=" hover:text-[#ca2124]">Contact</button>
-            <button className=" bg-[#ca2124] text-white p-3 rounded-full flex items-center gap-2 ">
-              <button>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                  />
-                </svg>
+            <Link to={"login"}>
+              <button className=" bg-[#ca2124] text-white p-3 rounded-full flex items-center gap-2 ">
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                    />
+                  </svg>
+                </button>
+                <span>Login</span>
               </button>
-              <span>Login</span>
-            </button>
+            </Link>
           </ul>
         </div>
         <div className=" lg:hidden flex">
