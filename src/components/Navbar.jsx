18a1476bba4ce25bfb37b15ff8930f19004b2609
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-12-09 01:41:44 +0200

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -119,23 +119,25 @@ const Navbar = () => {
                 Contact
               </div>
               <div className=" bg-[#ca2124] text-white cursor-pointer p-3 rounded-full w-fit flex items-center gap-2 ">
-                <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                    />
-                  </svg>
-                </button>
-                <span>Login</span>
+                <Link to={"login"}>
+                  <button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                      />
+                    </svg>
+                  </button>
+                  <span>Login</span>
+                </Link>
               </div>
             </ul>
           </motion.div>
