cac51cd54855344201367acaddd02b1f667b4527
tamani phiri <tamanigabriel0@gmail.com>
2023-12-10 14:09:57 +0200

    catalog and router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Catalog from "./pages/Catalog";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/catalog" Component={Catalog}/>
         <Route path="/login" Component={LoginPage}/>
       </Routes>
-      <Footer />
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,12 @@ import Header from "../components/Header";
 import Recommended from "../components/Recommended";
 import Services from "../components/Services";
 import Testimonial from "../components/Testimonial";
+import Navbar from '../components/Navbar'
 
 export default function HomePage() {
   return (
     <div>
+      <Navbar/>
       <Header />
       <Services />
       <Testimonial />
