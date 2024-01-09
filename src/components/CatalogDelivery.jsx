b0d1e1f988adc6a4006957460081ff52f113a54a
SamWaku <56162251+SamWaku@users.noreply.github.com>
2024-01-09 05:58:56 +0200

    local

--- src/components/CatalogDelivery.jsx
+++ src/components/CatalogDelivery.jsx
@@ -3,6 +3,13 @@ import groc from "../assets/catalog/grocery.png";
 import cloth from "../assets/catalog/laundry.png";
 import veg from "../assets/catalog/vegetable.png";
 
+// images
+// import boab from "../assets/catalog/baobab.png";
+import pine from "../assets/catalog/pinepples.png";
+import pot from "../assets/catalog/potatoes.png";
+import tom from "../assets/catalog/tomatoes.png";
+import wat from "../assets/catalog/watermelon.png";
+
 const CatalogDelivery = () => {
   return (
     <div className=" min-h-screen bg-red-50 px-20">
@@ -22,47 +29,98 @@ const CatalogDelivery = () => {
           <div className=" flex justify-between px-20 py-4">
             <a
               href="#"
-              className=" border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white"
+              className=" font-semibold border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white bg-white"
             >
               <img src={veg} alt="" className=" w-10" />
               Vegetables
             </a>
             <a
               href="#"
-              className=" border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white"
+              className=" font-semibold border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white bg-white"
             >
               <img src={groc} alt="" className=" w-10" />
               Other Groceries
             </a>
             <a
               href="#"
-              className=" border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white"
+              className=" font-semibold border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white bg-white"
             >
               <img src={acc} alt="" className=" w-10" />
               Accessories
             </a>
             <a
               href="#"
-              className=" border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white"
+              className=" font-semibold border border-red-400 rounded-md p-5 flex items-center gap-2 hover:bg-[#ca2124] hover:font-bold hover:text-white bg-white"
             >
               <img src={cloth} alt="" className=" w-10" />
               Clothing
             </a>
           </div>
         </div>
-        <div className=" px-10 items-center justify-between flex">
-          <a href="" className=" border rounded-md p-10">
-            Product 1
+        <div className=" px-10 items-start justify-between flex">
+          <a
+            href="#"
+            className=" border rounded-md flex justify-center overflow-hidden items-center flex-col"
+          >
+            <img
+              src={wat}
+              alt="watermelon"
+              loading="lazy"
+              className="object-scale-down max-h-full drop-shadow-md rounded-md m-auto w-52"
+            />
+            <div className=" py-8">
+              <p className="text-lg font-bold">Water Melon</p>
+              <p>K20 only</p>
+              <p>Order Now</p>
+            </div>
           </a>
-          <a href="" className=" border rounded-md p-10">
-            {" "}
-            Product 2
+          <a
+            href="#"
+            className=" border rounded-md flex justify-center overflow-hidden items-center flex-col"
+          >
+            <img
+              src={pot}
+              alt="watermelon"
+              loading="lazy"
+              className=" max-h-full drop-shadow-md rounded-md m-auto w-52 object-cover"
+            />
+            <div className=" py-8">
+              <p className="text-lg font-bold">Potatoes</p>
+              <p>K20 only</p>
+              <p>Order Now</p>
+            </div>
           </a>
-          <a href="" className=" border rounded-md p-10">
-            Product 3
+          <a
+            href="#"
+            className=" border rounded-md flex justify-center overflow-hidden items-center flex-col"
+          >
+            <img
+              src={pine}
+              alt="watermelon"
+              loading="lazy"
+              className="object-scale-down max-h-full drop-shadow-md rounded-md m-auto w-52"
+            />
+            <div className="">
+              <p className="text-lg font-bold">Pinepple</p>
+              <p>K20 only</p>
+              <p>Order Now</p>
+            </div>
           </a>
-          <a href="" className=" border rounded-md p-10">
-            Product 4
+          <a
+            href="#"
+            className=" border rounded-md flex justify-center overflow-hidden items-center flex-col"
+          >
+            <img
+              src={tom}
+              alt="watermelon"
+              loading="lazy"
+              className="object-scale-down max-h-full drop-shadow-md rounded-md m-auto w-52"
+            />
+            <div className="">
+              <p className="text-lg font-bold">Tomotoes</p>
+              <p>K20 only</p>
+              <p>Order Now</p>
+            </div>
           </a>
         </div>
       </div>
